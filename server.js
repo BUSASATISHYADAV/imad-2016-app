c26baaad7e0b5e88d1e63d20522900d73ed1b0cf
BUSASATISHYADAV <mdm16b006@iiitdm.ac.in>
2016-10-15 20:28:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,12 +14,12 @@ app.get('/article-one',function(req,res){
     res.send('Article one will be served from here');
 });
 
-app.get('/article-one',function(req,res){
-    res.send('Article one will be served from here');
+app.get('/article-two',function(req,res){
+    res.send('Article two will be served from here');
 });
 
-app.get('/article-one',function(req,res){
-    res.send('Article one will be served from here');
+app.get('/article-three',function(req,res){
+    res.send('Article three will be served from here');
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
